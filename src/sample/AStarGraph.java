1c9c423a21c2a0417f4019c0316d017725542b0e
Frederik <61193066+Sgtflint@users.noreply.github.com>
2020-04-28 16:33:58 +0200

    Has Updated the menuitems in GUI

--- src/sample/AStarGraph.java
+++ src/sample/AStarGraph.java
@@ -1,106 +1,136 @@
 package sample;
 
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 
 public class AStarGraph {
     private ArrayList<Vertex> vertices;
-    private Vertex Startvertex;
-
+    public boolean calculateAsManhattan = false;
     public AStarGraph() {
         vertices=new ArrayList<Vertex>();
     }
     public void addvertex(Vertex v) {
         vertices.add(v);
     }
-
     public void newconnection(Vertex v1, Vertex v2, Double dist) {
         v1.addOutEdge(v2,dist);
         v2.addOutEdge(v1,dist);
     }
-    public boolean A_Star(Vertex start, Vertex destination)
-    {   if (start==null || destination==null)
-        return false;
-        ArrayList<Vertex> Openlist = new ArrayList<Vertex>();
-        ArrayList<Vertex> Closedlist = new ArrayList<Vertex>();
-        Openlist.add(start);
-        Vertex Current;
-        ArrayList<Vertex> CurrentNeighbors;
+    public boolean A_Star(Vertex start, Vertex destination) {
+        if (start==null || destination==null)
+            return false;
+
+        PriorityQueue<Vertex> openList = new PriorityQueue<Vertex>();
+        ArrayList<Vertex> closedList = new ArrayList<Vertex>();
+        openList.offer(start);
+        Vertex current;
+        ArrayList<Vertex> currentNeighbors;
         Vertex Neighbor;
         //Initialize h with chosen heuristic
-        for (int i =0; i<vertices.size();i++)
-        {
-            vertices.get(i).seth(Manhattan(vertices.get(i),destination)); // initialize the sample.Manhattan distance, need implementation of the sample.Manhattan return function below
+        for (int i =0; i<vertices.size();i++) {
+            vertices.get(i).seth(Manhattan(vertices.get(i),destination));
         }
         start.setg(0.0);
-        start.calculatef(); // should be equal to the H value of my start
+        start.calculatef();
         //Start algorithm
         System.out.println("Start Algorithm");
+        System.out.println();
         //Implement the Astar algorithm
-        // Use Pseudo code
 
-        Openlist.add(Startvertex);
-        
-        while (!Openlist.isEmpty()){
-            Current = Fmin(Openlist);
-            for (int i = 0; i <Current.getNeighbours().size() -1 ; i++) {
-
-                double g = Current.getNeighbours().get(i).getg();
-                double h = Current.getNeighbours().get(i).geth();
-                double f = g + h;
-                Current.getNeighbours().get(i).setf(f);
-                Openlist.add(Current.getNeighbours().get(i));
-               // Closedlist.add(Current.getNeighbours().get(i));
+        while(openList.size()>0){
+            current = openList.remove();
+            System.out.println("Remove from open list: " + current.getid());
+            if(current.equals(destination)){
+                System.out.println(destination.getid());
+                System.out.println("Found destination, distance was: "+current.getg());
+                return true;
             }
+            closedList.add(current);
+            System.out.println("add to closedList" + current.getid());
 
+            for(Vertex vertex : current.getNeighbours()) {
+                System.out.println("We go to: " + vertex.getid());
+                double newWeight; // newWeight
+
+
+                // CALCULATE DISTANCE
+                if (calculateAsManhattan) {
+                    newWeight = current.getg() + Manhattan(current, vertex);
+                    System.out.println("we calculated the Manhattan to be: " + newWeight);
+                } else {
+                    newWeight = current.getg() + Euclidean(current, vertex);
+                    System.out.println("we calculated the Euclidean to be: " + newWeight);
+                }
+
+                // IF NEW CALCULATED DISTANCE is less than CALCULATED DISTANCE ON VERTEX
+                if (newWeight < vertex.getg()) {
+                    System.out.println("We set previous to be: " + current.getid());
+                    vertex.setPrev(current);
+                    vertex.setg(newWeight);
+
+                    System.out.println("We set the value: " + newWeight + ", to the vertex: " + vertex.getid());
+
+                    vertex.calculatef();
+
+                    // IF closedlist and opelist does not contain vertex
+                    if (!closedList.contains(vertex) && !openList.contains(vertex)) {
+                        openList.add(vertex);
+                    }
+
+                    if (openList.contains(vertex)) {
+                        openList.remove(vertex);
+                        openList.add(vertex);
+                    }
+
+                }
+            }
         }
+
+
+
+
+
+
 
         return false;
     }
-
-    public Vertex Fmin(ArrayList<Vertex> v){
-        double Fmin = 100000;
-        int y = 0;
-        for (int i = 0; i < v.size(); i++ ) {
-            if(v.get(i).getf() < Fmin) {
-                Fmin = v.get(i).getf();
-                y = i;
-            }
-        }
-        return v.get(y);
-    }
-
     public Double Manhattan(Vertex from,Vertex goal){
         //Implement this
+        System.out.println(goal.getx());
         double distance = Math.abs(goal.getx()-from.getx()) + Math.abs(goal.gety()-from.gety());
         return distance; // example of sample.Manhattan A*
     }
-
     public Double Euclidean( Vertex from,Vertex to){
         //Implement this
+        //System.out.println("From v: "+from.getid()+", fromX: "+from.getx()+", fromY: "+from.gety());
+        //System.out.println("To v: "+to.getid()+", ToX: "+to.getx()+", toY: "+to.gety());
         double x = to.getx()-from.getx();
         double y = to.getx()-from.gety();
         double distance = Math.sqrt(Math.pow(x, 2)+Math.pow(y, 2));
+        //System.out.println(distance);
         return distance; // sample.Euclidean
+
+
     }
 }
 
 class Vertex implements Comparable<Vertex>{
-    private String id; // just a name
-    private ArrayList<Vertex> Neighbours=new ArrayList<Vertex>(); // neighbouring array below
-    private ArrayList<Double> NeighbourDistance =new ArrayList<Double>(); // remember to put in stuff in both EX. A neighbour to a is b and c.
-    private Double f; // the complete estimate of how far there is from goal to origin vertex
-    private Double g; // something that is true, origin of path to any path
-    private Double h; // estimate from the Vertex to the Goal. G+H=F
-    private Integer x; // coordinates of our Vertex
-    private Integer y; // Coordinates of our Vertex
-    private Vertex prev =null; // path of prev-Current Vertex, this is the pathway
-    public Vertex(String id, int x_cor,int y_cor){
-        this.id=id;
-        this.x=x_cor;
-        this.y = y_cor;
-        setf(Double.POSITIVE_INFINITY); // How far the total journey
+    private String id;
+    private ArrayList<Vertex> Neighbours=new ArrayList<Vertex>();
+    private ArrayList<Double> NeighbourDistance =new ArrayList<Double>();
+    private Double f;
+    private Double g;
+    private Double h;
+    private Integer x;
+    private Integer y;
+    private Vertex prev =null;
+    public Vertex(String id, int x,int y){
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        f=Double.POSITIVE_INFINITY;
         g=Double.POSITIVE_INFINITY;
-        h=0.0; // has to be changed depending on which estimation we'd use, incase of the usage of ecldium or manhatten A*
+        h=0.0;
     }
     public void addOutEdge(Vertex toV, Double dist){
         Neighbours.add(toV);
@@ -112,67 +142,26 @@ class Vertex implements Comparable<Vertex>{
     public ArrayList<Double> getNeighbourDistance(){
         return NeighbourDistance;
     }
-    public String getid()
-    {
-        return id;
-    }
-    public Integer getx()
-    {
-        return x;
-    }
-    public Integer gety()
-    {
-        return x;
-    }
-    public Double getf()
-    {
-        return f;
-    }
-    public void calculatef()
-    {
-        setf(g+h);
-    }
-    public Double getg()
-    {
-        return g;
-    }
+    public String getid(){ return id;};
+    public Integer getx(){ return x; }
+    public Integer gety(){return y; }
+    public Double getf() { return f; }
+    public void calculatef(){ f=g+h; }
 
-    public void setg(Double newg)
-    {
-        g=newg;
-    }
-    public Double geth()
-    {
-        return h;
-    }
-    public void seth(Double estimate)
-    {
-        h=estimate;
-    }
-    public Vertex getPrev()
-    {
-        return prev;
-    }
-    public void setPrev(Vertex v)
-    {
-        prev=v;
-    }
-    public void printVertex()
-    {
+    public Double getg() { return g; }
+
+    public void setg(Double newg){ g=newg; }
+    public Double geth(){ return h; }
+    public void seth(Double estimate){ h=estimate; }
+    public Vertex getPrev() { return prev; }
+    public void setPrev(Vertex v){prev=v;}
+    public void printVertex(){
         System.out.println(id + " g: "+g+ ", h: "+h+", f: "+f);
     }
     @Override
-    public int compareTo(Vertex vertex) {
+    public int compareTo(Vertex o) {
 //Implement this
-// so that it can return 1 and -1 depending on the f value
-        if(this.getf() > vertex.getf())
-            return 1;
-            if (this.getf() < vertex.getf())
-                return -1;
+//
         return 0;
-    }
-
-    public void setf(Double f) {
-        this.f = f;
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,32 +1,17 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
 import java.util.Stack;
 
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("AStar_PathFinder");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
+public class Main {
 
     public static void main(String[] args) {
-        launch(args);
         AStarGraph MyMaze = new AStarGraph();
         // Make the graph provided to you in the diagram and table
         //The vertices must be constructed like A
+
         Vertex A = new Vertex("A", 0, 4);
         MyMaze.addvertex(A);
-        Vertex B = new Vertex("B", 0,4);
+        Vertex B = new Vertex("B", 1,7);
         MyMaze.addvertex(B);
         Vertex C = new Vertex("C", 4,0);
         MyMaze.addvertex(C);
@@ -44,7 +29,6 @@ public class Main extends Application {
         MyMaze.addvertex(I);
         Vertex J = new Vertex("J",11,5);
         MyMaze.addvertex(J);
-
         MyMaze.newconnection(A, B, 3.41);
         MyMaze.newconnection(A, C, 6.82);
         MyMaze.newconnection(B, D, 2.0);
@@ -58,32 +42,32 @@ public class Main extends Application {
         MyMaze.newconnection(G, I, 2.82);
         MyMaze.newconnection(H, J, 4.41);
         MyMaze.newconnection(I, J, 3.82);
-        //MyMaze.printGraph();
-        // vertexes and edges needs to return something
 
-            if (MyMaze.A_Star(A, J)) {
-                System.out.println("AStar_PathFinder");
-                Vertex pvertex = J; // has to be changed so that it is able to take start and goal.
-                //to print path -->
-                Stack<Vertex> Path = new Stack<>();
-                int limit = 0;
-                while (pvertex != null) {
-                    Path.push(pvertex);
-                    pvertex = pvertex.getPrev();
-                }
-                if (!Path.isEmpty())
-                    limit = Path.size();
-                for (int i = 0; i < limit - 1; i++)
-                    System.out.print(Path.pop().getid() + " - > ");
-                if (limit > 0)
-                    System.out.println(Path.pop().getid());
+        // TEST A STAR <-----------------
+        Vertex start = A;
+        Vertex slut = H;
 
-            } else
-                System.out.println("DID NOT FIND A PATH!!");
-
-
+        if(MyMaze.A_Star(start,slut))
+        {
+            int limit=0;
+            System.out.println("Found a path");
+            Vertex pvertex=slut;
+            Stack<Vertex> Path = new Stack<>();
+            while (pvertex!=null)
+            {
+                Path.push(pvertex);
+                pvertex=pvertex.getPrev();
+            }
+            if(!Path.isEmpty())
+                limit =Path.size();
+            for(int i=0;i<limit-1;i++)
+                System.out.print(Path.pop().getid() +" - > ");
+            if (limit>0)
+                System.out.println(Path.pop().getid());
 
         }
+        else
+            System.out.println("DID NOT FIND A PATH!!");
 
     }
-
+}
